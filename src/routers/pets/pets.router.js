08e9da46cefc71f5408dce297c93af4e753ab629
apamies <apamies94@outlook.com>
2023-03-27 09:31:21 +0200

    Minor fix - docs swagger

--- src/routers/pets/pets.router.js
+++ src/routers/pets/pets.router.js
@@ -31,7 +31,7 @@ router.get('/pets/species/average_age', (req, res) => {
         #swagger.description = "Get the average age of all pets of specific species."
 
         #swagger.parameters['species'] = {
-            in: 'body',
+            in: 'query',
             description: 'Species name',
             required: true
         }
